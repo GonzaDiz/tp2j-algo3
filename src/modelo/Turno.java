cb53523a9479a30b3ff9c51e505c12d4e53b4853
GonzaDiz <dizmartin.gonzalo@gmail.com>
2017-11-25 14:04:39 -0300

    empezando a modelar lo que seria el turno2

---
+++ src/modelo/Turno.java
@@ -0,0 +1,50 @@
+package modelo;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+import modelo.jugador.Jugador;
+
+public class Turno {
+	
+	private LinkedList<Jugador> jugadores;
+	private Dado dado1;
+	private Dado dado2;
+	
+	public Turno() {
+		this.jugadores = new LinkedList<>();
+		this.dado1 = new Dado();
+		this.dado2 = new Dado();
+	}
+	
+	public void agregarJugador(Jugador jugador) {
+		jugadores.add(jugador);
+	}
+	
+	public void asignarTurnos() {
+		Collections.shuffle(jugadores);
+	}
+
+	public Jugador getJugadorConTurno() {
+		return jugadores.removeFirst();
+	}
+
+	public void terminarTurno(Jugador jugador) {
+		jugadores.addLast(jugador);
+	}
+
+	public void arrojarDados(Jugador jugador) {
+		int valorDado1 = dado1.arrojar();
+		int valorDado2 = dado2.arrojar();
+		
+//		if (valorDado1 == valorDado2) {
+//			jugador.obtuvo(new TiroDoble(valorDado1,valorDado2));
+//		}
+//		else {
+//			jugador.obtuvo(new TiroSimple(valorDado1,valorDado2));
+//		}
+
+			
+		jugador.desplazar(valorDado1 + valorDado2);
+	}
+}
---
+++ src/modelo/excepciones/NoPuedeVolverAArrojarLosDadosException.java
@@ -0,0 +1,10 @@
+package modelo.excepciones;
+
+public class NoPuedeVolverAArrojarLosDadosException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
